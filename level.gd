6f324f81283648442bf3ec68244f68c121a202f9
Ron Wolf <ron-wolf@users.noreply.github.com>
2023-11-29 21:29:48 -0500

    Implement independent player scaling
    Have player scale independent from background scale.  Player scales based
    on last platform they touched, even while in the air.  Does not account
    for touching ceilings or walls belonging to background; detecting
    whether given collision was with floor is for next change.  Possible
    solution: just remove ceiling/wall hitboxes.

--- level.gd
+++ level.gd
@@ -1,44 +1,51 @@
 extends Node2D
+class_name Level
+
+enum ScaleDir {UP, DOWN}
 
 const BACKGROUND_SPEED = 50.0
-const BACKGROUND_ZOOM_MAX = 1.5
-const BACKGROUND_ZOOM_MIN = 0.5
-const BACKGROUND_ZOOM_INTERVAL = 0.1
-var background_zoom = 1.0
+const BG_ZOOM_MIN: int = -5
+const BG_ZOOM_MAX: int = +5
+const SCALE_FACTOR: float = 0.9
+var bg_zoom_lvl: int = 0
+var last_collisions: Array[KinematicCollision2D] = []
 
 @onready var background = $Background
 @onready var player = $Player
 
+static func calc_scale_vec(scale_lvl: int) -> Vector2:
+	var scale_factor: float = SCALE_FACTOR ** scale_lvl
+	return Vector2(scale_factor, scale_factor)
 
 func _ready():
 	pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
 #	var input_direction = Input.get_vector("bg_left", "bg_right", "bg_up", "bg_down")
 #
 #	background.velocity = input_direction * BACKGROUND_SPEED
 	
 	if Input.is_action_just_pressed("bg_scale_down"):
-		if background_zoom > BACKGROUND_ZOOM_MIN:
-			background_zoom -= BACKGROUND_ZOOM_INTERVAL
-			scale_platforms(Vector2(background_zoom, background_zoom))
-			scale_player(Vector2(background_zoom, background_zoom))
+		if bg_zoom_lvl > BG_ZOOM_MIN:
+			bg_zoom_lvl -= 1
+			scale_bg(ScaleDir.DOWN, Level.calc_scale_vec(bg_zoom_lvl))
 	if Input.is_action_just_pressed("bg_scale_up"):
-		if background_zoom < BACKGROUND_ZOOM_MAX:
-			background_zoom += BACKGROUND_ZOOM_INTERVAL
-			scale_platforms(Vector2(background_zoom, background_zoom))
-			scale_player(Vector2(background_zoom, background_zoom))
+		if bg_zoom_lvl < BG_ZOOM_MAX:
+			bg_zoom_lvl += 1
+			scale_bg(ScaleDir.UP, Level.calc_scale_vec(bg_zoom_lvl))
 	
 #	background.move_and_slide()
 	
-func scale_platforms(newScale):
-	background.scale = newScale
-#	for platform in background.platforms.get_children():
-#		platform.apply_scale(newScale)
-		
-func scale_player(newScale):
-	for platform in background.get_node("Platforms").get_children():
-		if platform.get_node("Area2D").overlaps_body(player):
-			print("Scaling player....")
-			player.scale = newScale
+func scale_bg(scale_dir: ScaleDir, new_scale: Vector2) -> void:
+	background.scale = new_scale
+	var fucking_dumb_stupid_dumb_condition_variable: bool = false
+	for any_bg_plat in $Background.platforms.get_children():
+		if fucking_dumb_stupid_dumb_condition_variable == true:
 			break
+		for collision in last_collisions:
+			if fucking_dumb_stupid_dumb_condition_variable == true:
+				break
+			var collider: Object = collision.get_collider()
+			if any_bg_plat.is_ancestor_of(collider):
+				player.scale_up_or_down(scale_dir)
+				fucking_dumb_stupid_dumb_condition_variable = true
--- player.gd
+++ player.gd
@@ -6,9 +6,12 @@ const FRICTION = 2000.0
 const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var scale_lvl = 0
 
 
 func _physics_process(delta):
+	scale = Level.calc_scale_vec(scale_lvl)
+	
 	apply_gravity(delta)
 	handle_jump()
 
@@ -16,7 +19,12 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	
-	move_and_slide()
+	var collided = move_and_slide()
+	if collided:
+		var level: Level = get_node("/root/Level")
+		level.last_collisions.clear()
+		for i in range(get_slide_collision_count()):
+			level.last_collisions.append(get_slide_collision(i))
 	
 func apply_gravity(delta):
 	# Add the gravity.
@@ -42,3 +50,10 @@ func apply_friction(input_axis, delta):
 	# Handle deceleration.
 	if not input_axis:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+func scale_up_or_down(scale_dir: Level.ScaleDir) -> void:
+	match scale_dir:
+		Level.ScaleDir.UP:
+			scale_lvl += 1
+		Level.ScaleDir.DOWN:
+			scale_lvl -= 1
