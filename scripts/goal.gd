5166d265678f6d1b5ca35f250fe6e1f34f97d035
Gregory Giovannini <36485898+greg300@users.noreply.github.com>
2023-11-30 20:55:36 -0500

    Level 4

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -3,5 +3,6 @@ extends Node2D
 const LEVELS = ["res://scenes/level1.tscn",
 				"res://scenes/level2.tscn",
 				"res://scenes/level3.tscn",
+				"res://scenes/level4.tscn",
 				"res://scenes/game_win.tscn"]
-const LEVEL_END = 4
+const LEVEL_END = 5
